7305324104249139d5e4c0746201d11748653117
AsriFox <asrifox@yandex.ru>
2023-12-18 20:49:23 +0300

    Refactor: converted `vscode` namespace import to named imports

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,10 @@
 'use strict';
 
-import * as vscode from 'vscode';
+import {
+    commands, window, workspace, tasks, l10n,
+    ExtensionContext, ConfigurationTarget,
+    Uri, TreeItem,
+} from 'vscode';
 import { basename } from 'path';
 import { PythonShell } from 'python-shell';
 import { exec, execOnFile } from './shellTask';
@@ -8,15 +12,15 @@ import * as blocks from './blockFilter';
 import * as modtool from './modtool';
 import { GNURadioModuleTreeDataProvider } from './moduleTree';
 
-export async function activate(context: vscode.ExtensionContext) {
+export async function activate(context: ExtensionContext) {
     const extId: string = context.extension.packageJSON.name;
 
-    const getConfig = <T>(key: string) => vscode.workspace.getConfiguration(extId).get<T>(key);
-    const setConfig = <T>(key: string, value?: T) => vscode.workspace.getConfiguration(extId)
-        .update(key, value, vscode.ConfigurationTarget.Global);
+    const getConfig = <T>(key: string) => workspace.getConfiguration(extId).get<T>(key);
+    const setConfig = <T>(key: string, value?: T) => workspace.getConfiguration(extId)
+        .update(key, value, ConfigurationTarget.Global);
 
-    const cwd = vscode.workspace.workspaceFolders?.length
-        ? vscode.workspace.workspaceFolders[0].uri.fsPath
+    const cwd = workspace.workspaceFolders?.length
+        ? workspace.workspaceFolders[0].uri.fsPath
         : undefined;
 
     /** 
@@ -24,7 +28,7 @@ export async function activate(context: vscode.ExtensionContext) {
      * 
      * This command runs `gnuradio-companion` in the shell.
      */
-    const openGnuradioCompanion = vscode.commands.registerCommand(
+    const openGnuradioCompanion = commands.registerCommand(
         `${extId}.openGnuradioCompanion`,
         () => {
             const cmd = getConfig<string>('companion.cmd') ?? 'gnuradio-companion';
@@ -36,9 +40,9 @@ export async function activate(context: vscode.ExtensionContext) {
      * 
      * This command runs `gnuradio-companion %f` in the shell, opening the selected file `%f`.
      */
-    const editInGnuradioCompanion = vscode.commands.registerCommand(
+    const editInGnuradioCompanion = commands.registerCommand(
         `${extId}.editInGnuradioCompanion`,
-        (fileUri?: vscode.Uri) => {
+        (fileUri?: Uri) => {
             const cmd = getConfig<string>('companion.cmd') ?? 'gnuradio-companion';
             execOnFile(`"${cmd}"`, fileUri, '.grc');
         });
@@ -48,9 +52,9 @@ export async function activate(context: vscode.ExtensionContext) {
      * 
      * This command runs `grcc %f` in the shell, producing a Python executable in the same folder as the selected file `%f`.
      */
-    const compileFlowgraph = vscode.commands.registerCommand(
+    const compileFlowgraph = commands.registerCommand(
         `${extId}.compileFlowgraph`,
-        (fileUri?: vscode.Uri) => {
+        (fileUri?: Uri) => {
             const cmd = getConfig<string>('compiler.cmd') ?? 'grcc';
             execOnFile(`"${cmd}"`, fileUri, '.grc');
         });
@@ -60,9 +64,9 @@ export async function activate(context: vscode.ExtensionContext) {
      * 
      * This command runs `grcc -r %f` in the shell, producing a Python executable in the same folder as the selected file `%f` and running it.
      */
-    const runFlowgraph = vscode.commands.registerCommand(
+    const runFlowgraph = commands.registerCommand(
         `${extId}.runFlowgraph`,
-        (fileUri?: vscode.Uri) => {
+        (fileUri?: Uri) => {
             const cmd = getConfig<string>('compiler.cmd') ?? 'grcc';
             execOnFile(`"${cmd}" -r`, fileUri, '.grc');
         });
@@ -72,23 +76,23 @@ export async function activate(context: vscode.ExtensionContext) {
         editInGnuradioCompanion,
         compileFlowgraph,
         runFlowgraph,
-        vscode.tasks.onDidEndTaskProcess(e => {
+        tasks.onDidEndTaskProcess(e => {
             if (e.execution.task.source !== 'gnuradio') {
                 return;
             }
             if (e.exitCode && e.exitCode !== 0) {
-                vscode.window.showErrorMessage(
+                window.showErrorMessage(
                     `Task finished with error code ${e.exitCode}; check the terminal output for details`);
             } else if (e.execution.task.detail?.startsWith('grcc')) {
                 // TODO: C++ flowgraph?
                 // TODO: read task parameters to find the compiled file
-                vscode.window.showInformationMessage('Flowgraph compilation was successfull');
+                window.showInformationMessage('Flowgraph compilation was successfull');
             }
         }),
     );
 
-    const outputChannel = vscode.window.createOutputChannel(context.extension.packageJSON.displayName);
-    const scriptPath = vscode.Uri.joinPath(context.extensionUri, 'src', 'modtool').fsPath;
+    const outputChannel = window.createOutputChannel(context.extension.packageJSON.displayName);
+    const scriptPath = Uri.joinPath(context.extensionUri, 'src', 'modtool').fsPath;
     const execModtool: modtool.ModtoolClosure = async (command: string, ...args: string[]) => {
         outputChannel.appendLine(`\n[Running] gr_modtool ${command} ${args.join(' ')}`);
         return PythonShell.run(
@@ -100,7 +104,7 @@ export async function activate(context: vscode.ExtensionContext) {
     };
 
     context.subscriptions.push(
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.createModule`,
             () => modtool.createModule(outputChannel, scriptPath)),
     );
@@ -111,75 +115,75 @@ export async function activate(context: vscode.ExtensionContext) {
     // Detect OOT module in the current working directory
     const moduleName: string = (await modtool.getModuleInfo(execModtool, true))?.modname;
     if (!moduleName) {
-        const noModule = () => vscode.window.showErrorMessage("No GNURadio Module detected in the open workspace");
-        context.subscriptions.push(vscode.commands.registerCommand(`${extId}.getModuleInfo`, noModule));
+        const noModule = () => window.showErrorMessage("No GNURadio Module detected in the open workspace");
+        context.subscriptions.push(commands.registerCommand(`${extId}.getModuleInfo`, noModule));
         return;
     }
-    vscode.commands.executeCommand('setContext', `${extId}.moduleFound`, true);
+    commands.executeCommand('setContext', `${extId}.moduleFound`, true);
 
     // Command Palette
     context.subscriptions.push(
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.getModuleInfo`,
             () => modtool.getModuleInfo(execModtool)),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.createBlock`,
             () => modtool.createBlock(execModtool, context.extensionUri, cwd, moduleName)),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.createPythonBindings`,
             () => modtool.createPythonBindings(execModtool, cwd, moduleName)),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.renameBlock`,
             () => modtool.renameBlock(execModtool, cwd, moduleName)),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.disableBlock`,
             () => modtool.disableBlock(execModtool, cwd, moduleName)),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.removeBlock`,
             () => modtool.removeBlock(execModtool, cwd, moduleName)),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.convertXmlToYaml`,
             () => modtool.convertXmlToYaml(execModtool, cwd, moduleName)),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.makeYamlFromImpl`,
             () => modtool.makeYamlFromImpl(execModtool, cwd, moduleName)),
     );
 
     // File Explorer Context Menu
     context.subscriptions.push(
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.createPythonBindingsInExplorer`,
-            (blockUri: vscode.Uri) => {
+            (blockUri: Uri) => {
                 if (!blockUri) {
-                    vscode.window.showErrorMessage('No file provided!');
+                    window.showErrorMessage('No file provided!');
                     return;
                 }
                 if (!blocks.filterCppBlocks(blockUri.fsPath)) {
-                    vscode.window.showErrorMessage(`Invalid file type: expected a header (.h), found ${basename(blockUri.fsPath)}`);
+                    window.showErrorMessage(`Invalid file type: expected a header (.h), found ${basename(blockUri.fsPath)}`);
                 }
                 const blockName = blocks.mapCppBlocks(blockUri.fsPath);
                 return modtool.createPythonBindings(execModtool, cwd, moduleName, blockName);
             }),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.convertXmlToYamlInExplorer`,
-            (blockUri: vscode.Uri) => {
+            (blockUri: Uri) => {
                 if (!blockUri) {
-                    return vscode.window.showErrorMessage('No file provided!');
+                    return window.showErrorMessage('No file provided!');
                 }
                 if (!blocks.filterXmlBlocks(blockUri.fsPath)) {
-                    return vscode.window.showErrorMessage(`Invalid file type: expected XML, found ${basename(blockUri.fsPath)}`);
+                    return window.showErrorMessage(`Invalid file type: expected XML, found ${basename(blockUri.fsPath)}`);
                 }
                 const blockName = blocks.mapGrcBlocks('.xml')(blockUri.fsPath);
                 return modtool.convertXmlToYaml(execModtool, cwd, moduleName, blockName);
             }),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.makeYamlFromImplInExplorer`,
-            (blockUri: vscode.Uri) => {
+            (blockUri: Uri) => {
                 if (!blockUri) {
-                    return vscode.window.showErrorMessage('No file provided!');
+                    return window.showErrorMessage('No file provided!');
                 }
                 if (!blocks.filterCppBlockImpl(blockUri.fsPath)) {
-                    return vscode.window.showErrorMessage(`Invalid file type: expected C++ source, found ${basename(blockUri.fsPath)}`);
+                    return window.showErrorMessage(`Invalid file type: expected C++ source, found ${basename(blockUri.fsPath)}`);
                 }
                 const blockName = blocks.mapCppBlockImpl(blockUri.fsPath);
                 return modtool.makeYamlFromImpl(execModtool, cwd, moduleName, blockName);
@@ -193,17 +197,17 @@ export async function activate(context: vscode.ExtensionContext) {
      */
     const checkXml = async () => {
         const xmlBlocks = blocks.getXmlBlocks(cwd, moduleName);
-        vscode.commands.executeCommand('setContext', `${extId}.xmlFound`, xmlBlocks.length > 0);
+        commands.executeCommand('setContext', `${extId}.xmlFound`, xmlBlocks.length > 0);
         if (!xmlBlocks.length) {
             return;
         }
-        const yes = vscode.l10n.t("Yes"), no = vscode.l10n.t("No"), dontShowAgain = vscode.l10n.t("Don't Show Again");
-        let updateAll = await vscode.window.showInformationMessage(
+        const yes = l10n.t("Yes"), no = l10n.t("No"), dontShowAgain = l10n.t("Don't Show Again");
+        let updateAll = await window.showInformationMessage(
             'XML block definitions found. Update them to YAML?', yes, no, dontShowAgain);
         if (updateAll === 'Yes') {
             await execModtool('update', '--complete');
-            vscode.commands.executeCommand('setContext', `${extId}.xmlFound`, false);
-            updateAll = await vscode.window.showInformationMessage(
+            commands.executeCommand('setContext', `${extId}.xmlFound`, false);
+            updateAll = await window.showInformationMessage(
                 'Updated block definitions written to "grc/".', dontShowAgain);
         }
         if (updateAll === dontShowAgain) {
@@ -214,7 +218,7 @@ export async function activate(context: vscode.ExtensionContext) {
         checkXml();
     }
     context.subscriptions.push(
-        vscode.workspace.onDidChangeConfiguration(e => {
+        workspace.onDidChangeConfiguration(e => {
             if (e.affectsConfiguration('gnuradio-integration.modtool.checkXml') &&
                 getConfig<boolean>('modtool.checkXml') === true) {
                 checkXml();
@@ -225,9 +229,9 @@ export async function activate(context: vscode.ExtensionContext) {
     const moduleTree = new GNURadioModuleTreeDataProvider(cwd, moduleName);
 
     const registerTreeItemAlias = (alias: string, command: string) =>
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.${alias}`,
-            (item?: vscode.TreeItem) => {
+            (item?: TreeItem) => {
                 if (!item) {
                     if (!moduleTree.treeView.selection.length) {
                         return;
@@ -237,12 +241,12 @@ export async function activate(context: vscode.ExtensionContext) {
                 if (!item.resourceUri) {
                     return;
                 }
-                return vscode.commands.executeCommand(
+                return commands.executeCommand(
                     command, item.resourceUri,
                 );
             });
 
-    const getBlockFromTreeItem = (item?: vscode.TreeItem) => {
+    const getBlockFromTreeItem = (item?: TreeItem) => {
         if (!item && moduleTree.treeView.selection.length) {
             item = moduleTree.treeView.selection[0];
         }
@@ -254,31 +258,31 @@ export async function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(
         moduleTree,
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.refreshView`,
             moduleTree.refresh,
             moduleTree),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.renameBlockInTree`,
-            (item?: vscode.TreeItem) => {
+            (item?: TreeItem) => {
                 const blockName = getBlockFromTreeItem(item);
                 return modtool.renameBlock(execModtool, cwd, moduleName, blockName);
             }),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.removeBlockInTree`,
-            (item?: vscode.TreeItem) => {
+            (item?: TreeItem) => {
                 const blockName = getBlockFromTreeItem(item);
                 return modtool.removeBlock(execModtool, cwd, moduleName, blockName);
             }),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.createPythonBindingsInTree`,
-            (item?: vscode.TreeItem) => {
+            (item?: TreeItem) => {
                 const blockName = getBlockFromTreeItem(item);
                 return modtool.createPythonBindings(execModtool, cwd, moduleName, blockName);
             }),
-        vscode.commands.registerCommand(
+        commands.registerCommand(
             `${extId}.convertXmlToYamlInTree`,
-            (item?: vscode.TreeItem) => {
+            (item?: TreeItem) => {
                 const blockName = getBlockFromTreeItem(item);
                 return modtool.convertXmlToYaml(execModtool, cwd, moduleName, blockName);
             }),
