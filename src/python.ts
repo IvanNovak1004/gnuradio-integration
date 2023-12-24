fe0c94cf28f8b176bd42503f36ad3cfb6c9899de
AsriFox <asrifox@yandex.ru>
2023-12-24 18:53:17 +0300

    Fixed PythonShell not using scriptPath to call scripts; Fixed PythonShell not outputting stdout/stderr contents on exit; Fixed PythonShellError not being in its prototype chain.

--- src/python.ts
+++ src/python.ts
@@ -1,6 +1,7 @@
 import { OutputChannel, window } from 'vscode';
 import { execSync, spawn } from 'child_process';
-import { platform } from 'os';
+import { EOL, platform } from 'os';
+import { resolve } from 'path';
 
 export class PythonShellError extends Error {
     log?: string;
@@ -11,7 +12,7 @@ export class PythonShellError extends Error {
         this.name = 'PythonShellError';
         this.log = log;
         this.exitCode = exitCode;
-        Object.setPrototypeOf(this, PythonShellError);
+        Object.setPrototypeOf(this, PythonShellError.prototype);
     }
 }
 
@@ -20,13 +21,13 @@ export class PythonShell {
         readonly pythonPath: string,
         readonly scriptPath: string,
         readonly outputChannel: OutputChannel,
-    ) {}
+    ) { }
 
     public static default(scriptPath: string, displayName: string) {
         let pythonPath = 'python';
         if (platform() !== 'win32') {
             try {
-                pythonPath = execSync(`command -v ${pythonPath}`, { encoding: 'utf8' });
+                pythonPath = execSync(`command -v ${pythonPath}`, { encoding: 'utf8' }).trim();
             } catch (_) {
                 pythonPath = 'python3';
             }
@@ -35,20 +36,28 @@ export class PythonShell {
     }
 
     run(command: string[], cwd?: string, encoding?: BufferEncoding) {
+        command[0] = resolve(this.scriptPath, command[0]);
         return new Promise<string>((resolve, reject) => {
             const py = spawn(this.pythonPath, command, { cwd });
+            let stdout: string[] = [], stderr: string[] = [];
             py.stdout.setEncoding(encoding ?? 'utf8');
-            py.stdout.on('data', (data: string) => this.outputChannel.appendLine(data));
+            py.stdout.on('data', (data: string) => {
+                stdout.push(data);
+                this.outputChannel.appendLine(data);
+            });
             py.stderr.setEncoding(encoding ?? 'utf8');
-            py.stderr.on('data', (data: string) => this.outputChannel.appendLine(data));
-            py.on('error', err => reject(new PythonShellError(err.message, py.stdout.read())));
+            py.stderr.on('data', (data: string) => {
+                stderr.push(data);
+                this.outputChannel.appendLine(data);
+            });
+            py.on('error', err => reject(new PythonShellError(err.message, stderr.join(EOL))));
             py.on('exit', code => {
                 if (!code || code === 0) {
-                    resolve(py.stdout.read());
+                    resolve(stdout.join(EOL));
                 } else {
                     reject(new PythonShellError(
                         `Script '${command.join(' ')}' exited with error code ${code}`,
-                        py.stdout.read(), code));
+                        stderr.join(EOL), code));
                 }
             });
         });
